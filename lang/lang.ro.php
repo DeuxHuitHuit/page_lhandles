ce8fb7d3bf0ed52b6d0f17e26ee6e6fae30267ae
Vlad <vlad_micutul@yahoo.com>
2011-02-16 22:53:55 +0200

    Added Romanian translation

---
+++ lang/lang.ro.php
@@ -0,0 +1,29 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad.ghita@xandergroup.ro',
+			'website' => ''
+		),
+		'release-date' => '2011-02-16'
+	);
+
+	/**
+	 * Page LHandles
+	 */
+	$dictionary = array(
+		'Unknown Lang' => 
+		'Limbă Necunoscută',
+
+		'Localised Title' => 
+		'Titlu localizat',
+
+		'Localised URL Handle' => 
+		'Handle URL localizat',
+
+		'<code>Page LHandles</code> depends on <code>%s</code>. Make sure you have this extension installed and enabled.' => 
+		'<code>Page LHandles</code> depinde de <code>%s</code>. Asiguraţi-vă că această extensie există şi este activată.',
+
+	);
