1b6bff1838a90a84bed4268fc877b3ac4834865c
Vlad <vlad_micutul@yahoo.com>
2011-06-28 20:15:11 +0300

    Added French language

---
+++ lang/lang.fr.php
@@ -0,0 +1,29 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name'			=> 'Solutions Nitriques',
+			'website'		=> 'http://www.nitriques.com/open-source/',
+			'email'			=> 'open-source (at) nitriques.com'
+		),
+		'release-date' => '2011-06-28'
+	);
+
+	/**
+	 * Page LHandles
+	 */
+	$dictionary = array(
+		'Unknown Lang' => 
+		'Langue inconnue',
+
+		'Localised Title' => 
+		'Titre localisé',
+
+		'Localised URL Handle' => 
+		'Handle d\'URL localisé',
+
+		'<code>Page LHandles</code> depends on <code>%s</code>. Make sure you have this extension installed and enabled.' => 
+		'<code>Page LHandles</code> dépends de <code>%s</code>. Assurez-vous d\'avoir cette extension installée et activée.',
+
+	);
