62258f04002fe9360e521b49f0f6169674dccf39
Vlad <vlad_micutul@yahoo.com>
2011-06-28 18:20:10 +0300

    Updated for compatibility with Language Redircet 1.0.2

--- data-sources/data.plh_page.php
+++ data-sources/data.plh_page.php
@@ -1,74 +1,74 @@
 <?php
 
-require_once(TOOLKIT . '/class.datasource.php');
+	require_once(TOOLKIT . '/class.datasource.php');
+		
+	Class datasourceplh_page extends Datasource{
+			
+		public function about(){
+				return array(
+					'name' => 'PLH Page',
+					'author' => array(
+						'name' => 'Vlad Ghita',
+						'email' => 'vlad_micutul@yahoo.com'),
+					'version' => '1.0',
+					'release-date' => '2011-02-16',
+					'description' => 'From Page LHandles extension. Retrieves the current pages\' and it\'s parents localised titles and handles in all supported languages.');
+		}
+			
+		public function allowEditorToParse(){
+			return false;
+		}
 	
-Class datasourceplh_page extends Datasource{
-		
-	public function about(){
-			return array(
-				'name' => 'PLH Page',
-				'author' => array(
-					'name' => 'Vlad Ghita',
-					'email' => 'vlad_micutul@yahoo.com'),
-				'version' => '1.0',
-				'release-date' => '2011-02-16',
-				'description' => 'From Page LHandles extension. Retrieves the current pages\' and it\'s parents localised titles and handles in all supported languages.');
-	}
-		
-	public function allowEditorToParse(){
-		return false;
-	}
-
-    public function grab(&$param_pool=NULL){
-    	$result = new XMLElement('plh-page');
-		
-    	$pages = PageLHandles::getPageAscendingLine();
-    	$languageCodes = LanguageRedirect::instance()->getSupportedLanguageCodes();
-    	$languageCodesH = PageLHandles::getLanguageCodes_();
-		
-    	$i = 0;
-		
-    	$result->appendChild(
-    		$this->_addPageXML($pages, $i, $languageCodes, $languageCodesH)
-    	);
-
-        return $result;
-    }
-    
-    private function _addPageXML($pages, $i, $languageCodes, $languageCodesH) {
-    	
-    	$pageXML = new XMLElement(
-    			'page', 
-    			null,
-		    	array(
-		    		'handle' => $pages[$i]['handle'],
-					'id' => $pages[$i]['id']
-    			)
-    	);
-    	
-    	foreach ($languageCodes as $key => $language) {
-    		$itemXML = new XMLElement(
-					'item', 
-    				htmlspecialchars ( $pages[$i]['page_lhandles_t_'.$languageCodesH[$key] ] , ENT_NOQUOTES ), // espace &
-    				array(
-						'lang' => $language,
-						'handle' => $pages[$i][ 'page_lhandles_h_'.$languageCodesH[$key] ]
-    				)
-    		);
-
-    		$pageXML->appendChild($itemXML);
-    	}
-    	
-    	if ( $i < count($pages)-1 ) {
-    		
-	    	$pageXML->appendChild(
-	    		$this->_addPageXML($pages, $i + 1, $languageCodes, $languageCodesH)
+	    public function grab(&$param_pool=NULL){
+	    	$result = new XMLElement('plh-page');
+			
+	    	$pages = PageLHandles::getPageAscendingLine();
+	    	$languageCodes = LanguageRedirect::instance()->getSupportedLanguageCodes();
+	    	$languageCodesH = PageLHandles::getLanguageCodes_();
+			
+	    	$i = 0;
+			
+	    	$result->appendChild(
+	    		$this->_addPageXML($pages, $i, $languageCodes, $languageCodesH)
+	    	);
+	
+	        return $result;
+	    }
+	    
+	    private function _addPageXML($pages, $i, $languageCodes, $languageCodesH) {
+	    	
+	    	$pageXML = new XMLElement(
+	    			'page', 
+	    			null,
+			    	array(
+			    		'handle' => $pages[$i]['handle'],
+						'id' => $pages[$i]['id']
+	    			)
 	    	);
 	    	
-    	}
-    	
-    	return $pageXML;
-    	
-    }
+	    	foreach ($languageCodes as $key => $language) {
+	    		$itemXML = new XMLElement(
+						'item', 
+	    				htmlspecialchars ( $pages[$i]['page_lhandles_t_'.$languageCodesH[$key] ] , ENT_NOQUOTES ), // espace &
+	    				array(
+							'lang' => $language,
+							'handle' => $pages[$i][ 'page_lhandles_h_'.$languageCodesH[$key] ]
+	    				)
+	    		);
+	
+	    		$pageXML->appendChild($itemXML);
+	    	}
+	    	
+	    	if ( $i < count($pages)-1 ) {
+	    		
+		    	$pageXML->appendChild(
+		    		$this->_addPageXML($pages, $i + 1, $languageCodes, $languageCodesH)
+		    	);
+		    	
+	    	}
+	    	
+	    	return $pageXML;
+	    	
+	    }
     
 }
