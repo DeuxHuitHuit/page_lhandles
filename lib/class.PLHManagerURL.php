277895d1a7989c83ec3b65926bdee19fdb61a607
nitriques <nitriques@users.noreply.github.com>
2017-05-17 22:57:46 -0400

    Unix EOL conversion

--- lib/class.PLHManagerURL.php
+++ lib/class.PLHManagerURL.php
@@ -2,11 +2,7 @@
 
 	if( !defined('__IN_SYMPHONY__') ) die('<h2>Error</h2><p>You cannot directly access this file</p>');
 
-
-
 	require_once(EXTENSIONS.'/frontend_localisation/lib/class.FLang.php');
-
-
 
 	final class PLHManagerURL
 	{
